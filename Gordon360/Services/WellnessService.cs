95da391bbd102888b9c3bb470abcb28c3f3d6a2e
Ariarley Dospassos <Ari.Dospassos@gordon.edu>
2020-06-07 15:09:18 -0400

    created back end for wellness check

---
+++ Gordon360/Services/WellnessService.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Gordon360.Models;
+using Gordon360.Models.ViewModels;
+using Gordon360.Repositories;
+using Gordon360.Exceptions.CustomExceptions;
+using System.Data.SqlClient;
+using Gordon360.Services.ComplexQueries;
+using System.Diagnostics;
+
+namespace Gordon360.Services
+{
+    public class WellnessService : IWellnessService
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public WellnessService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        /// <summary>
+        /// gets status of wellness check
+        /// </summary>
+        /// <param name="id">id</param>
+        /// <returns>boolean if found, null if not found</returns>
+
+        public IEnumerable<WellnessViewModel> GetStatus(string id)
+        {
+            var query = _unitOfWork.AccountRepository.FirstOrDefault(x => x.gordon_id == id);
+            if (query == null)
+            {
+                throw new ResourceNotFoundException() { ExceptionMessage = "The account was not found." };
+            }
+
+            var idParam = new SqlParameter("@ID", id);
+            var result = true //RawSqlQuery<WellnessViewModel>.query("VICTORY_PROMISE_BY_STUDENT_ID @ID", idParam); //run stored procedure
+            if (result == null)
+            {
+                throw new ResourceNotFoundException() { ExceptionMessage = "The data was not found." };
+            }
+            
+            return result;
+
+        }
+    }
+}
